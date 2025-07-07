5492a2ae0feef733139dc261ee0edc7fccaca2cf
Aysenursvs <sivasligiln@gmail.com>
2025-07-07 14:59:04 +0300

    some errors are fixed

--- bot/booking/booking.py
+++ bot/booking/booking.py
@@ -1,5 +1,5 @@
 import os
-from . import constans as const
+from .constants import BASE_URL
 from selenium import webdriver
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -8,10 +8,12 @@ chromedriver_path = os.path.join(current_dir, "..", "..", "chromedriver")
 class Booking(webdriver.Chrome):
     def __init__(self, executable_path=chromedriver_path):
        self.driver_path = executable_path
+       os.environ["PATH"] += os.pathsep + self.driver_path
        super(Booking, self).__init__()
 
     def land_first_page(self):
-        self.get(const.BASE_URL)
-        self.implicitly_wait(10)
+        self.get(BASE_URL)
+        
+
 
 
--- bot/run.py
+++ bot/run.py
@@ -1,5 +1,11 @@
 from booking.booking import Booking
-
+import time
 
 booking = Booking()
 booking.land_first_page()
+
+# Tarayıcıyı 10 saniye açık tut
+time.sleep(10)
+
+# Tarayıcıyı kapat
+booking.quit()
