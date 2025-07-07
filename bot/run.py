17a4d63ff2aef4c977b03218774e86f4edc25a56
Aysenursvs <sivasligiln@gmail.com>
2025-07-07 15:30:30 +0300

    with block added

--- bot/run.py
+++ bot/run.py
@@ -1,9 +1,9 @@
 from booking.booking import Booking
 import time
 
-bot = Booking()
-bot.land_first_page()
-    
+with Booking() as bot:
+    bot.land_first_page()
+    print("Exiting...")
 
 # Tarayıcıyı 10 saniye açık tut
 
