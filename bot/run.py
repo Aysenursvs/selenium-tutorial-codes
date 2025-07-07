6d28574c866baebf6093ddaa03cb6eeafa964ce4
Aysenursvs <sivasligiln@gmail.com>
2025-07-07 15:35:39 +0300

    time.sleep added

--- bot/run.py
+++ bot/run.py
@@ -1,8 +1,10 @@
 from booking.booking import Booking
 import time
 
+
 with Booking() as bot:
     bot.land_first_page()
+    time.sleep(10)  # Wait for 10 seconds to keep the browser open
     print("Exiting...")
 
 # Tarayıcıyı 10 saniye açık tut
